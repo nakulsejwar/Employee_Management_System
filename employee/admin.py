f77b5e5bdb2573f9100ebba14bb7811a9d425d1d
nakulsejwar <nakulsejwar02@gmail.com>
2025-04-08 11:59:16 +0530

    Initial commit

---
+++ employee/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from employee.models import Employee,Attendance,Notice
+
+
+# Register your models here.
+admin.site.register(Employee)
+admin.site.register(Attendance)
+admin.site.register(Notice)
